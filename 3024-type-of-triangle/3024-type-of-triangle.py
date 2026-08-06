5f1f73f5b2371a08f4108165cb5675b9397422be
Dhinesh K <luckydhinesh2007@gmail.com>
2026-08-06 15:27:52 +0530

    Time: 0 ms (100%), Space: 12.2 MB (88.51%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.py
+++ 3024-type-of-triangle/3024-type-of-triangle.py
@@ -4,10 +4,12 @@ class Solution(object):
                nums[1] + nums[2] <= nums[0] or
                nums[0] + nums[2] <= nums[1]):
                return "none"
-            a=len(set(nums))
-            if a==1:
-                return "equilateral"
-            elif a==2:
-                return "isosceles"
             else:
-                return "scalene"
+
+                a=len(set(nums))
+                if a==1:
+                    return "equilateral"
+                elif a==2:
+                    return "isosceles"
+                else:
+                    return "scalene"
